8f6585832678019881ee1c47849445da9aa83774
Victor Weiss <bonjour@victor-weiss.fr>
2021-12-10 16:57:16 +0100

    Filtre personnalisé TWIG

---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('price', [$this, 'getPrice']),
+        ];
+    }
+
+    // public function getFunctions(): array
+    // {
+    //     return [
+    //         new TwigFunction('function_name', [$this, 'doSomething']),
+    //     ];
+    // }
+
+    public function getPrice(int|float $number): string
+    {
+        return number_format($number, 2, ',', ' ') . ' €';
+    }
+}
