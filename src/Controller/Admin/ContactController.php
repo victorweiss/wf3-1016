dac88c8a143a719e5e8aea7d291e73df1de72d53
Victor Weiss <bonjour@victor-weiss.fr>
2021-12-15 14:13:23 +0100

    Admin ContactController.php : index + view

--- src/Controller/Admin/ContactController.php
+++ src/Controller/Admin/ContactController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Contact;
 use App\Repository\ContactRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/admin/contact')]
 class ContactController extends AbstractController
 {
-    #[Route('/admin/contact', name: 'admin_contact')]
+    #[Route('', name: 'admin_contact_index')]
     public function index(ContactRepository $contactRepository): Response
     {
         $contacts = $contactRepository->findBy([], ['id' => 'DESC']);
 
         return $this->render('admin/contact/index.html.twig', [
             'contacts' => $contacts
+        ]);
+    }
+
+    #[Route('/{id}', name: 'admin_contact_view')]
+    public function view(Contact $contact): Response
+    {
+        return $this->render('admin/contact/view.html.twig', [
+            'contact' => $contact,
         ]);
     }
 }
