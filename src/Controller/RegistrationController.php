1beda91cbb207d36f1be6809d4e5d68b4af2ca35
Victor Weiss <bonjour@victor-weiss.fr>
2022-01-04 10:10:01 +0100

    Security email confirmation

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,15 +6,17 @@ use App\Entity\User;
 use App\Entity\UserToken;
 use App\Form\RegistrationFormType;
 use App\Security\AppAuthenticator;
+use App\Service\NotifyService;
 use DateTime;
 use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
@@ -23,7 +25,8 @@ class RegistrationController extends AbstractController
     public function register(
         Request $request,
         UserPasswordHasherInterface $userPasswordHasher,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        NotifyService $notifyService
     ): Response
     {
         $user = new User();
@@ -46,7 +49,21 @@ class RegistrationController extends AbstractController
             $entityManager->persist($userToken);
 
             $entityManager->flush();
+
             // do anything else you need here, like send an email
+            $email = (new TemplatedEmail())
+                ->to($user->getEmail())
+                ->subject("Validez votre compte")
+                ->htmlTemplate('registration/email/register_verification_email.email.twig')
+                ->context([
+                    'user' => $user,
+                    'link' => $this->generateUrl(
+                        'register_verification_email',
+                        ['token' => $userToken->getToken()],
+                        UrlGeneratorInterface::ABSOLUTE_URL
+                    ),
+                ]);
+            $notifyService->sendEmail($email);
 
             return $this->redirectToRoute('register_confirmation');
         }
